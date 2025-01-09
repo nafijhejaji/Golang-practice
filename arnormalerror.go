5d662ca1add2764a1d58ac43eb14000730f9f327
nafijhejaji <nsnafijhejaji@gmail.com>
2025-01-09 08:24:11 +0600

    playing with variables and comment out the ourcomes

---
+++ arnormalerror.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 284
+	a = 20
+
+	fmt.Print(a)
+
+	a = 100
+	// Here a = 100 will not be printed because a = 100 has been assigned after the first assignment of a = 20.
+}
--- variable.go
+++ variable.go
@@ -4,5 +4,17 @@ import "fmt"
 
 func main() {
 	a := 13
-	fmt.Println(a)
+	a = 100 // we cannot use (:) the second time we want to assign a value to a same variable.
+	// Line 7 will print 100 not 13.
+	fmt.Println(a) //the basic one
+
+	var c int = 15
+	var d = 40.56      // we can also use this way to declare a variable. Go doesn't require you to declare the type of the variable, it can infer it from the value you assign to it.
+	e := "hello world" // we can also use this way to declare a variable. Go doesn't require you to declare the type of the variable, it can infer it from the value you assign to it.
+	g := true          // the first time what kind of value you assign to the variable, the type of the variable will be the same as the value you assign to it. In this case, the type of g is bool. then you can't assign a string to g. g = "hello world" will cause an error. because the type of g is bool not string.
+	g = false
+	fmt.Println(c, d, e) // This will assign the values to the variables and print them out on the same line.
+	fmt.Println(g)       // This will print the value of g which is false. Not true.
+
+	const p = 100 // This is a constant variable. You can't change the value of a constant variable. If you try to change the value of a constant variable, you will get an error.
 }
