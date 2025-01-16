499fcb13a0e6cea741d07eb82debcc8678152215
nafijhejaji <nsnafijhejaji@gmail.com>
2025-01-16 09:07:10 +0600

    variables and switches

--- variable.go
+++ variable.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
+func summation1(type1 int, type2 int) {
+	fmt.Println(type1 + type2)
+
+}
+
+func summation2(type1 float64, type2 float64) {
+	fmt.Println(type1 + type2)
+
+}
+
+//in this summation function , if we want to use same datatype like float and float or int and int then we can use this function. But if we want to use int and float64 then we need to transform the int into float using floa64() function.
+
 func main() {
-	a := 13
-	a = 100 // we cannot use (:) the second time we want to assign a value to a same variable.
-	// Line 7 will print 100 not 13.
-	fmt.Println(a) //the basic one
+	x := 15
+	y := 5
+	z := 20.5
+	summation1(x, y)
+	summation2(float64(x), z)
 
-	var c int = 15
-	var d = 40.56      // we can also use this way to declare a variable. Go doesn't require you to declare the type of the variable, it can infer it from the value you assign to it.
-	e := "hello world" // we can also use this way to declare a variable. Go doesn't require you to declare the type of the variable, it can infer it from the value you assign to it.
-	g := true          // the first time what kind of value you assign to the variable, the type of the variable will be the same as the value you assign to it. In this case, the type of g is bool. then you can't assign a string to g. g = "hello world" will cause an error. because the type of g is bool not string.
-	g = false
-	fmt.Println(c, d, e) // This will assign the values to the variables and print them out on the same line.
-	fmt.Println(g)       // This will print the value of g which is false. Not true.
-
-	const p = 100 // This is a constant variable. You can't change the value of a constant variable. If you try to change the value of a constant variable, you will get an error.
 }
